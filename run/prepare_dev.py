66319eb5e2855dcab7d4213a427d6bc0fe075b4d
tubo213 <yuuki.32040808@gmail.com>
2023-10-02 14:45:41 +0900

    streaming

--- run/prepare_dev.py
+++ run/prepare_dev.py
@@ -8,10 +8,12 @@ from omegaconf import DictConfig
 @hydra.main(config_path="conf", config_name="prepare_data", version_base="1.2")
 def main(cfg: DictConfig):
     data_dir = Path(cfg.dir.data_dir)
-    series_df = pl.read_parquet(data_dir / "train_series.parquet")
-    sample_200_series_ids = series_df.get_column("series_id").unique().sample(200)
+    series_df = pl.scan_parquet(data_dir / "train_series.parquet")
+    sample_200_series_ids = (
+        series_df.select("series_id").unique().collect(streaming=True).sample(200).get_column("series_id")
+    )
     dev_series_df = series_df.filter(pl.col("series_id").is_in(sample_200_series_ids))
-    dev_series_df.write_parquet(data_dir / "dev_series.parquet")
+    dev_series_df.sink_parquet(data_dir / "dev_series.parquet")
     print("Done")
 
 
