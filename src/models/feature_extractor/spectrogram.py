ebdc4be256c93ef1cef5b8473c95af95c538a6cf
tubo213 <yuuki.32040808@gmail.com>
2023-10-21 16:07:17 +0900

    出力するtimelengthを指定可能に

--- src/models/feature_extractor/spectrogram.py
+++ src/models/feature_extractor/spectrogram.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
 import torchaudio.transforms as T
@@ -18,13 +20,27 @@ class SpecNormalize(nn.Module):
 
 
 class SpecFeatureExtractor(nn.Module):
-    def __init__(self, n_fft: int, hop_length: int):
+    def __init__(
+        self,
+        n_fft: int,
+        hop_length: int,
+        win_length: Optional[int] = None,
+        out_size: Optional[int] = None,
+    ):
         super().__init__()
         self.feature_extractor = nn.Sequential(
-            T.Spectrogram(n_fft=n_fft, hop_length=hop_length),
+            T.Spectrogram(n_fft=n_fft, hop_length=hop_length, win_length=win_length),
             T.AmplitudeToDB(top_db=80),
             SpecNormalize(),
         )
+        self.out_size = out_size
+
+        if self.out_size is not None:
+            self.pool = nn.AdaptiveAvgPool2d((None, self.out_size))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.feature_extractor(x)
+        img = self.feature_extractor(x)
+        if self.out_size is not None:
+            img = self.pool(img)
+
+        return img
