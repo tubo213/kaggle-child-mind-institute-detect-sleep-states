47ace2c6b706026ed8664d376c6307bf656a1617
tubo213 <yuuki.32040808@gmail.com>
2023-11-14 01:46:28 +0900

    LSTMのdownsamplingをConv1Dに変更

--- src/models/feature_extractor/lstm.py
+++ src/models/feature_extractor/lstm.py
@@ -11,10 +11,18 @@ class LSTMFeatureExtractor(nn.Module):
         hidden_size: int,
         num_layers: int,
         bidirectional: bool,
+        stride: int,
         out_size: Optional[int] = None,
     ):
         super().__init__()
         self.fc = nn.Linear(in_channels, hidden_size)
+        self.conv = nn.Conv1d(
+            in_channels=hidden_size,
+            out_channels=hidden_size,
+            kernel_size=stride,
+            stride=stride,
+            padding=1,
+        )
         self.height = hidden_size * (2 if bidirectional else 1)
         self.lstm = nn.LSTM(
             input_size=hidden_size,
@@ -38,12 +46,14 @@ class LSTMFeatureExtractor(nn.Module):
             torch.Tensor: (batch_size, out_chans, height, time_steps)
         """
         # x: (batch_size, in_channels, time_steps)
+        x = self.fc(x.transpose(1, 2))  # x: (batch_size, time_steps, hidden_size)
+        x = self.conv(x.transpose(1, 2))  # x: (batch_size, hidden_size, time_steps)
+
         if self.out_size is not None:
-            x = x.unsqueeze(1)  # x: (batch_size, 1, in_channels, time_steps)
-            x = self.pool(x)  # x: (batch_size, 1, in_channels, output_size)
-            x = x.squeeze(1)  # x: (batch_size, in_channels, output_size)
-        x = x.transpose(1, 2)  # x: (batch_size, output_size, in_channels)
-        x = self.fc(x)  # x: (batch_size, output_size, hidden_size)
+            x = x.unsqueeze(1)  # x: (batch_size, 1, hidden_size, time_steps)
+            x = self.pool(x)  # x: (batch_size, 1, hidden_size, output_size)
+            x = x.squeeze(1)  # x: (batch_size, hidden_size, output_size)
+        x = x.transpose(1, 2)  # x: (batch_size, output_size, hidden_size)
         x, _ = self.lstm(x)  # x: (batch_size, output_size, hidden_size * num_directions)
         x = x.transpose(1, 2)  # x: (batch_size, hidden_size * num_directions, output_size)
         x = x.unsqueeze(1)  # x: (batch_size, out_chans, hidden_size * num_directions, time_steps)
